435e3f7b6253139bea047f99cbda6026273fe2ed
Flu3nt <gnedyalkov94@gmail.com>
2023-03-24 16:12:51 +0200

    Add register form state

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,21 +1,48 @@
+import { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const onLoginChange = (e) => {
+    setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const onLoginSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(values);
+  };
   return (
-    <div>
-      <h2 className="page-title">Register</h2>
-      <form action="post">
+    <section>
+      <h2>Register</h2>
+      <form action="post" onSubmit={onLoginSubmit}>
         <div>
           <label htmlFor="username">Username: </label>
-          <input type="text" name="username" id="username" />
+          <input
+            value={values.username}
+            onChange={onLoginChange}
+            type="text"
+            name="username"
+            id="username"
+          />
         </div>
         <div>
           <label htmlFor="password">Password: </label>
-          <input type="text" name="password" id="password" />
+          <input
+            value={values.password}
+            onChange={onLoginChange}
+            type="password"
+            name="password"
+            id="password"
+          />
         </div>
         <input type="submit" value="Login" />
       </form>
-    </div>
+    </section>
   );
 };
 
