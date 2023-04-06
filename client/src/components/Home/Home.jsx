cbd56f2349bf6ff68d51fdaa08177d589ab4ecf2
Flu3nt <gnedyalkov94@gmail.com>
2023-04-06 13:21:41 +0300

    Fix: Home hero section layout and resizing

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,23 +3,28 @@ import "./Home.css";
 
 const Home = ({ paintings }) => {
   return (
-    <>
-      <h1>Home of the Art</h1>
+    <main>
+      <div className="hero">
+        <div>
+          <h1>Welcome to the Art Hub</h1>
+          <p>Share your art and your interpretations with others</p>
+        </div>
 
-      {/* <img
-        className="hero-img"
-        src="https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQ-FvbbAq5IaJUhtwxXEwY0D-jiZju02ejnNHx_bQWL_27GF3srhwJgqusMAqKh3QqU"
-        alt=""
-      /> */}
+        <div className="hero-img-container">
+          <img
+            className="hero-img"
+            src="https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQ-FvbbAq5IaJUhtwxXEwY0D-jiZju02ejnNHx_bQWL_27GF3srhwJgqusMAqKh3QqU"
+            alt="hero image: the mona lisa"
+          />
+        </div>
+      </div>
 
-      <p>Share your art and your interpretations with others</p>
-
-      <h3>Here are some the most notable paintings in our life time</h3>
+      <h2>Catalogue</h2>
 
       <div>
         <Paintings paintings={paintings} />
       </div>
-    </>
+    </main>
   );
 };
 
