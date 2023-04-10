ac1310c06f21de67100c8beca2e4c16d474e16d1
Flu3nt <gnedyalkov94@gmail.com>
2023-04-10 16:38:46 +0300

    Create a user manager with ability to register users with a hashed password

--- myServer/controllers/userController.js
+++ myServer/controllers/userController.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const userManager = require("../managers/userManager");
 
 router.get("/", (req, res) => {
   res.status(200).json({ msg: "User route" });
+});
+
+router.post("/register", async (req, res) => {
+  const { name, username, email, password, repeatPassword } = req.body;
+
+  if (password !== repeatPassword) {
+    throw new Error(`Passwords do not match.`);
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await userManager.create({
+    ...req.body,
+    password: hashedPassword,
+  });
+
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
