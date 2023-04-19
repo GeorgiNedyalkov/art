69ec8f9b4c582ee97569565ee0b6776369a1eeea
Flu3nt <gnedyalkov94@gmail.com>
2023-04-19 11:35:48 +0300

    Add github to footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import "./Footer.css";
+
 const Footer = () => {
   return (
     <footer>
@@ -7,6 +8,14 @@ const Footer = () => {
           Developed by
           <a href="www.georginedyalkov.com"> Georgi Nedyalkov &copy;</a>
         </p>
+        <div>
+          <a href="https://github.com/GeorgiNedyalkov">
+            <img
+              className="social-icon"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Github-circle_%28CoreUI_Icons_v1.0.0%29.svg/2048px-Github-circle_%28CoreUI_Icons_v1.0.0%29.svg.png"
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
