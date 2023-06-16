657c88437a851e7d01ccdd98eb65981c16975527
Flu3nt <gnedyalkov94@gmail.com>
2023-06-16 14:17:07 +0300

    feature: add a home route

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,9 @@ const artController = require("./controllers/artController");
 const userController = require("./controllers/userController");
 const artistController = require("./controllers/artistController");
 
+router.use("/", (req, res) => {
+  res.send("Hello arthub server");
+});
 router.use("/art-pieces", artController);
 router.use("/users", userController);
 router.use("/artists", artistController);
