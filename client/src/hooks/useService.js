a0e155c1fd045363cd5a797d6f79d8be180348ff
Flu3nt <gnedyalkov94@gmail.com>
2023-06-19 10:57:57 +0300

    refactor: add client folder to server folder

--- client/src/hooks/useService.js
+++
@@ -1,9 +0,0 @@
-import { useAuthContext } from "../context/AuthContext";
-
-export const useService = (serviceFactory) => {
-  const { token } = useAuthContext();
-
-  const service = serviceFactory(token);
-
-  return service;
-};
